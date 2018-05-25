c5417b5c5482a0b33bf81092f1eff0ac77dcaff8
Ravi Kishore Thella <ravikishorethella@gmail.com>
2018-05-25 16:22:02 -0400

    added the prices and the insurance of the make

--- js/app.js
+++ js/app.js
@@ -1,4 +1,6 @@
 // variables
+const form = document.getElementById("request-quote");
+const html = new HTMLUI();
 
 // eventListeners
 
@@ -7,13 +9,80 @@ eventListeners();
 function eventListeners() {
   // on page load
   document.addEventListener("DOMContentLoaded", () => {
-    const html = new HTMLUI();
     html.displayYears();
+  });
+
+  //   when the form is submitted
+  form.addEventListener("submit", function(e) {
+    //   prevent the page from loading
+    e.preventDefault();
+
+    //   reading the values from the form
+    const make = document.getElementById("make").value;
+    const year = document.getElementById("year").value;
+
+    // check if the radio button is checked
+    const level = document.querySelector('input[name="level"]:checked').value;
+
+    //     if any of the form field is empty then return an error
+    if (make === "" || year === "" || level === "") {
+      // using the global html variable
+      html.displayError("All the fields are mandatory");
+    } else {
+      //     make the quotation here
+      const insurance = new Insurance(make, year, level);
+      const price = insurance.calculateQuotation(insurance);
+    }
   });
 }
 
 // objects
 
+// object related to the quotation insurance
+function Insurance(make, year, level) {
+  this.make = make;
+  this.year = year;
+  this.level = level;
+}
+
+// creating a method to the insurance object using the prototype
+Insurance.prototype.calculateQuotation = function(insurance) {
+  let price;
+  const base = 2000;
+
+  // get the make
+  const make = insurance.make;
+
+  // The values are just some random numbers
+
+  switch (make) {
+    case "1":
+      price = base * 1.15;
+      break;
+    case "2":
+      price = base * 1.05;
+      break;
+    case "3":
+      price = base * 1.35;
+      break;
+  }
+
+  //   get the year
+  const year = insurance.year;
+  //   differnec between the current year and the selected year
+  const difference = this.getYearDifference(year);
+
+  //   each year the inssurance  price is going to get 3% cheaper
+  price = price - difference * 3 * price / 100;
+  console.log(price);
+};
+
+// creating a difference between the years
+Insurance.prototype.getYearDifference = function(year) {
+  return new Date().getFullYear() - year;
+};
+
+// object related to the html
 function HTMLUI() {}
 
 // adding a method to the object using the prototype
@@ -32,6 +101,23 @@ HTMLUI.prototype.displayYears = function() {
     option.textContent = i;
     selectedYears.appendChild(option);
   }
+};
 
-  console.log(selectedYears);
+// displaying the error message
+HTMLUI.prototype.displayError = function(message) {
+  const div = document.createElement("div");
+  // adding a class to the div
+  div.classList = "error";
+
+  // inserting the error message
+  div.innerHTML = `
+            <p>${message}</p>
+      `;
+  // inserting the message above the form fields
+  form.insertBefore(div, document.querySelector(".form-group"));
+
+  //   timeout for the error message
+  setTimeout(() => {
+    document.querySelector(".error").remove();
+  }, 3000);
 };
